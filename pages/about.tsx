64989cc8a2bae21d90f02c7f0f3f7ff8582d3996
Wellington Lopes <w.camara.lopes@gmail.com>
2021-12-09 15:56:21 -0300

    About page

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Navigation from "@components/navigation"
+
+const About = () => {
+  return (
+    <>
+      <Navigation />
+      <header>
+        <h2>About</h2>
+      </header>
+    </>
+  )
+}
+
+export default About
